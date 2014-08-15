7e3576d56f83e8bae110e5aab09af670b4648d20
Sean Olson <seanolson@google.com>
2014-08-15 13:03:07 -0700

    use realpath to get repo root path

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #! /usr/bin/env bash
 
+realpath() {
+  (
+    TARGET_FILE="$1"
+    cd "$(dirname "$TARGET_FILE")"
+    TARGET_FILE="$(basename "$TARGET_FILE")"
+
+    while [ -L "$TARGET_FILE" ]; do
+      TARGET_FILE="$(readlink "$TARGET_FILE")"
+      cd "$(dirname "$TARGET_FILE")"
+      TARGET_FILE="$(basename "$TARGET_FILE")"
+    done
+
+    PHYS_DIR="$(pwd -P)"
+    echo "$PHYS_DIR/$TARGET_FILE"
+  )
+}
+
+REPO_ROOT=$(dirname $(realpath $BASH_SOURCE))
+
 # Link Gnome Shell extensions.
 ln -s \
-  ./extensions/extra-panels@darkxst.feathertop.org \
+  "$REPO_ROOT/extensions/extra-panels@darkxst.feathertop.org" \
   ~/.local/share/gnome-shell/extensions/extra-panels@darkxst.feathertop.org
