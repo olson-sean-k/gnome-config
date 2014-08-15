88fb8c4c79f40c270ce0ccd0611d961fbbb17741
Sean Olson <seanolson@google.com>
2014-08-15 12:45:23 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# Link Gnome Shell extensions.
+ln -s \
+  ./extensions/extra-panels@darkxst.feathertop.org \
+  ~/.local/share/gnome-shell/extensions/extra-panels@darkxst.feathertop.org
