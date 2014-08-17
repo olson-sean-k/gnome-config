353b4b861c671ec65279b94c25c6094184de35a1
Sean Olson <olson.sean.k@gmail.com>
2014-08-17 16:07:53 -0700

    ln all files in gnome-shell/extensions/

--- install-gnome-shell.sh
+++ install-gnome-shell.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 # Link Gnome Shell extensions.
-ln -sf \
-  "$REPO_ROOT/gnome-shell/extensions/extra-panels@darkxst.feathertop.org" \
-  ~/.local/share/gnome-shell/extensions/extra-panels@darkxst.feathertop.org
+for path in "$REPO_ROOT/gnome-shell/extensions"/* ; do
+  ln -sf \
+    "$path" \
+    ~/".local/share/gnome-shell/extensions/$(basename $path)"
+done
