f2ffe52ac9312e9056753e32cd604d8fb5f022d4
Sean Olson <olson.sean.k@gmail.com>
2014-08-18 11:58:29 -0700

    use --no-dereference to prevent recursive links

--- install-gnome-shell.sh
+++ install-gnome-shell.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 # Link Gnome Shell extensions.
+shopt -s nullglob
 for path in "$REPO_ROOT/gnome-shell/extensions"/* ; do
-  ln -sf \
+  ln -fns \
     "$path" \
     ~/".local/share/gnome-shell/extensions/$(basename $path)"
 done
